8fdc11b131c464903ad0023ac3609858bb74c2b0
Leo Nishida <leo.crescencio.nishida@gmail.com>
2025-04-16 10:19:52 -0700

    4/16/25 10:19

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -8,8 +8,9 @@ chosenword = wordbank[rand.randint(0, len(wordbank) - 1)]
 letters = []
 board = []
 wordinput = ""
-solved = 0
+solved = len(chosenword)
 attemptlimit = 5
+attempt = 0
 
 #set up functions
 
@@ -20,18 +21,23 @@ def setup():
         board.append("_")
 
 def ui():
+    global wordinput
     print(board)
     board.clear()
     wordinput = input("Guess? ")
 
 def computation():
+
     if len(wordinput) == len(chosenword):
         for let in range(len(chosenword)):
             for corlet in range(len(wordinput)):
                 if chosenword[let] == wordinput[corlet]:
+                    global solved
                     board.append(u'\u2713')
+                    solved =+ 1
     else:
         print("Wrong number of letters!")
+    print(board)
 
 def game():
     setup()
@@ -39,11 +45,16 @@ def game():
         if solved != 1:
             ui()
             computation()
-            print("Last guess: ", wordinput)
+            print("Last guess: " + wordinput)
     print(board)
-    print("Congratulations you solved it!")
+    if solved == 1:
+        print("Congratulations you solved it!")
+    else: 
+        print("You lost :( Nice try!")
     playagain = input("Play again? Y/N")
     if playagain == "Y":
+        board.clear()
+        solved = 0
         game()
 
 game()
